8bb572dc0f9e48333d3a4bd1fafaef111a22894e
Piotr Kasprzyk <kaspiotr@gmail.com>
2017-10-17 16:52:31 +0200

    Add new file to repository

---
+++ Set1_Ex5/Set1_Ex5.cpp
@@ -0,0 +1,22 @@
+/*
+5. Napisać program obliczający pierwiastek całkowitoliczbowy z liczby naturalnej
+korzystając z zależności 1+3+5+... = n^2.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(n>=0){
+        n-=i;
+        i+=2;
+    }
+    cout<<((i-1)/2)-1;
+
+    return 0;
+}
