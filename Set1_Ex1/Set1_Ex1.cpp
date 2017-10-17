979612de503953237c2bd789b5eb9cd11c0ff619
Piotr Kasprzyk <kaspiotr@gmail.com>
2017-10-17 07:16:46 +0200

    Add new files to repository

---
+++ Set1_Ex1/Set1_Ex1.cpp
@@ -0,0 +1,25 @@
+/*
+1. Napisać program generujący elementy ciągu Fibonaciego mniejsze od miliona.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+const int N = 1000000;
+
+int main(){
+
+    int F1=1, F2=1, F3;
+
+    while(F1<N){
+        cout<<F1<<endl;
+
+        F3 = F1 + F2;
+        F1 = F2;
+        F2 = F3;
+    }
+
+    return 0;
+}
+
