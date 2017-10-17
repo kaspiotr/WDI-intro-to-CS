03d94d981bb8e7bd82d0b9251f9d9befa2312cd1
Piotr Kasprzyk <kaspiotr@gmail.com>
2017-10-17 18:22:54 +0200

    Add new file to repository

---
+++ Set1_Ex6/Set1_Ex6.cpp
@@ -0,0 +1,23 @@
+/*
+6. Napisać program wypisujący podzielniki liczby.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, div=1;
+    cin>>n;
+
+    while(n > div){
+        if(n%div == 0){
+            cout<<div<<"\t";
+        }
+        div += 1;
+    }
+
+    cout<<n<<endl;
+
+    return 0;
+}
