6bc7953077831899b53267df4f379218ab1f0cae
Piotr Kasprzyk <kaspiotr@gmail.com>
2017-10-18 18:19:39 +0200

    Add new file to repository

---
+++ Set1_Ex10/Set1_Ex10.cpp
@@ -0,0 +1,29 @@
+/*
+10. Napisać program wyznaczający największy wspólny dzielnik 3 zadanych liczb.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int Euklides(int a, int b){
+    while(a*b != 0){
+        if(a>b) a=a%b;
+        else b=b%a;
+    }
+
+    if(a == 0) return b;
+    return a;
+}
+
+int main(){
+
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int nwd = Euklides(a,b);
+
+    cout<<Euklides(nwd,c)<<endl;
+
+    return 0;
+}
