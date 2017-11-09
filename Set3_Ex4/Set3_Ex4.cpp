a1367dcc4b2797b224ae6edb7737dfeb971f0714
kaspiotr <kaspiotr@gmail.com>
2017-11-09 20:21:16 +0100

    Add solution of the exercise

---
+++ Set3_Ex4/Set3_Ex4.cpp
@@ -0,0 +1,55 @@
+//author: @kaspiotr (Piotr Kasprzyk)
+
+/*
+4. Napisać program wypełniający tablicę int t[MAX] liczbami naturalnymi 1-1000 i sprawdzający
+czy każdy element tablicy zawiera co najmniej jedną cyfrę nieparzystą.
+MAX jest zdefiniowaną stałą.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10; //na potrzeby testow
+
+//funckja, ktora sprawdza czy co najmniej jedna cyfra podanej liczy jest nieparzysta
+bool isAtLeastOneOdd(int a){
+    bool isThereOddDig = false;
+    int digit;
+
+    while(a != 0){
+        digit = a%10;
+        if(digit%2 == 1) isThereOddDig = true;
+        a/=10;
+    }
+
+    return isThereOddDig;
+}
+
+int main(){
+    srand(time(NULL));
+
+    int tab[MAX];
+
+    //int tab[MAX] = {9,9,9,9,9,9,9,9,9,9};
+
+    for(int i=0; i<MAX; i++){
+        tab[i]=rand()%1000+1;
+    }
+
+    for(int i=0; i<MAX; i++){
+        cout<<tab[i]<<"\t";
+    }
+    cout<<endl;
+
+    bool allElsHaveOddDig = true;
+    for(int i=0; i<MAX; i++){
+        if(!isAtLeastOneOdd(tab[i])) allElsHaveOddDig = false;
+    }
+
+    if(allElsHaveOddDig) cout<<"TAK"<<endl;
+    else cout<<"NIE"<<endl;
+
+    return 0;
+}
+
