8df84ca785b8f8bfe40705c4296210c4122b4f92
kaspiotr <kaspiotr@gmail.com>
2017-11-20 20:57:09 +0100

    Add new set of exercises (contents) to the repository

---
+++ Set4_Ex1/Set4_Ex1.cpp
@@ -0,0 +1,20 @@
+//author: @kaspiotr (Piotr Kasprzyk)
+
+/*
+1. Dana jest tablica int t[MAX][MAX]. Proszę napisad funkcję wypełniającą tablicę liczbami naturalnymi po spirali.
+MAX jest zdefiniowaną stałą.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
+
+
+
