29987db9eec5c7d93e465d8b24ec2040a339e509
kaspiotr <kaspiotr@gmail.com>
2018-01-11 15:25:01 +0100

    Add new files to repository

--- Set8_Ex1/Set8_Ex1.cpp
+++ Set8_Ex1/Set8_Ex1.cpp
@@ -1,4 +1,78 @@
-//
-// Created by kaspiotr on 1/10/18.
-//
+//author: @kaspiotr (Piotr Kasprzyk)
 
+/*
+1. Proszę napisać funkcję wstawiającą na koniec listy nowy element. Do funkcji należy przekazać
+wskazanie na pierwszy element listy oraz wstawianą wartość.
+*/
+
+//wersja bez wartownika (version without sentry)
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int val;
+    Node *next;
+};
+
+//funckja pomocnicza do wyswietlania lancucha odsylaczowego
+void printList(Node *first) {
+
+    while(first != NULL) {
+        cout<<first->val<<"\t";
+        first=first->next;
+    }
+
+    cout<<endl;
+}
+
+void addOneAtTheEnd(Node* &first, int newVal) {
+    if(first == NULL) {
+        first = new Node;
+        first->val = newVal;
+        first->next = NULL;
+        return;
+    }
+
+    Node *curr = first;
+    Node *prev = NULL;
+
+    while(curr != NULL) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    curr = new Node;
+    curr->val = newVal;
+    curr->next = NULL;
+    prev->next = curr;
+}
+
+int main() {
+
+    Node *first2 = new Node;
+    first2->val = 9;
+    Node *curr2 = new Node;
+    first2->next = curr2;
+    curr2->val = 8;
+    Node *curr22 = new Node;
+    curr2->next = curr22;
+    curr22->val = 7;
+    Node *curr23 = new Node;
+    curr22->next = curr23;
+    curr23->val = 6;
+    curr23->next = NULL;
+
+    printList(first2);
+
+    cout<<"Give a natural number that will be added at the of the list"<<endl;
+    int value;
+    cin>>value;
+
+    addOneAtTheEnd(first2, value);
+
+    printList(first2);
+
+    return 0;
+}
--- Set8_Ex1/Set8_Ex1_ver2.cpp
+++ Set8_Ex1/Set8_Ex1_ver2.cpp
@@ -1,4 +1,70 @@
-//
-// Created by kaspiotr on 1/10/18.
-//
+//author: @kaspiotr (Piotr Kasprzyk)
 
+/*
+1. Proszę napisać funkcję wstawiającą na koniec listy nowy element. Do funkcji należy przekazać
+wskazanie na pierwszy element listy oraz wstawianą wartość.
+*/
+
+//wersja z wartownikiem (version with sentry)
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int val;
+    Node *next;
+};
+
+//funckja pomocnicza do wyswietlania lancucha odsylaczowego
+void printList(Node *first) {
+
+    while(first != NULL) {
+        cout<<first->val<<"\t";
+        first=first->next;
+    }
+
+    cout<<endl;
+}
+
+//argument -linked list with sentry
+void addOneAtTheEnd(Node* &first) {
+    Node * curr = first->next;
+    if(curr == NULL) {
+        curr = new Node;
+
+    }
+}
+
+int main() {
+
+    //linked list with sentry
+    Node *first = new Node;
+    first->next = curr1;
+    Node *curr1 = new Node;
+    curr1->val = 9;
+    Node *curr2 = new Node;
+    first2->next = curr2;
+    curr2->val = 8;
+    Node *curr3 = new Node;
+    curr2->next = curr3;
+    curr3->val = 7;
+    Node *curr4 = new Node;
+    curr3->next = curr4;
+    curr4->val = 6;
+    curr4->next = NULL;
+
+    printList(first);
+
+    cout<<"Give a natural number that will be added at the of the list"<<endl;
+    int value;
+    cin>>value;
+
+    addOneAtTheEnd(first, value);
+
+    printList(first);
+
+    return 0;
+
+    return 0;
+}
